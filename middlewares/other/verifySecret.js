4a48d131465955aa648025fac0ef20983b3ced61
Sefinek <sefinek5916@gmail.com>
2024-11-26 21:23:12 +0100

    Update verifySecret.js

--- middlewares/other/verifySecret.js
+++ middlewares/other/verifySecret.js
@@ -1,8 +1,8 @@
 module.exports = (req, res, next) => {
-	const { access_token: secretHeader } = req.headers;
-	const { shardSecret: secretBody } = req.body;
-	if (secretHeader !== process.env.SHARD_SECRET || secretBody !== process.env.SHARD_SECRET) {
-		console.warn(`An incorrect shardSecret was received! Client IP: ${req.ip}`);
+	const { access_token: headerSecret } = req.headers;
+	const { shardSecret: bodySecret } = req.body;
+	if (headerSecret !== process.env.SHARD_SECRET || bodySecret !== process.env.SHARD_SECRET) {
+		console.warn(`An incorrect shardSecret (req.headers.access_token || req.body.shardSecret) was received! Client IP: ${req.ip}`);
 		return res.sendStatus(403);
 	}
 
